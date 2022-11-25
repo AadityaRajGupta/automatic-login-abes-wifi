d6d0c3cbf4a0148fb6c7aa6334bef8529d67631b
Aaditya Raj Gupta <67413825+AadityaRajGupta@users.noreply.github.com>
2022-11-25 10:57:34 +0530

    Add files via upload
    This javascript file has the commands to be used in tampermonkey chrome extension script.

---
+++ wifi_script.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://192.168.1.254:8090/httpclient.html
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=1.254
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    document.querySelector("#username").setAttribute("value","enter username here");
+    document.querySelector("#password").setAttribute("value","enter your password here");
+    document.querySelector("#loginbutton").click();
+})();
